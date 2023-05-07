bf8e4fe64e970a768706ccc96149cebeb99e6f25
Flavius <fcateloiu08@gmail.com>
2023-05-07 11:12:02 +0200

    Creación del proyecto para de Spring donde se creara la API REST.

---
+++ src/main/java/com/project/proyectofinalonepiece/ProyectoFinalOnePieceApplication.java
@@ -0,0 +1,13 @@
+package com.project.proyectofinalonepiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalOnePieceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFinalOnePieceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/proyectofinalonepiece/ProyectoFinalOnePieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.proyectofinalonepiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalOnePieceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
